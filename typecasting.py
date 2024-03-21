b4cf690012e08b216f831efa8067d7bdb0acb20f
DeepakSivalingam <deepaksivalingam5@gmail.com>
2024-03-21 19:21:35 +0530

    type casting updated

---
+++ typecasting.py
@@ -0,0 +1,30 @@
+#type casting in python 
+
+a=39.3
+print(type(a))  # here we converting the float type to integer 
+b=int(a)
+print(type(b))
+
+c=3
+d=float(c)     #here we converting the integer to float
+print(type(d))
+
+
+    # constructors used in python for typecasting 
+    # int()
+    #float()
+    #str()
+    
+    
+    #  , is used for placing the integer with the string value 
+    
+    
+x=29
+y=3
+c=x+y
+print("total :",c)
+
+x=29
+y=3
+c=x+y
+print("total :"+str(c))
