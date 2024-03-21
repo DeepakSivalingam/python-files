55f595ee0d7194e752ba78d8f3fcc4b1f2a15556
DeepakSivalingam <deepaksivalingam5@gmail.com>
2024-03-21 21:29:42 +0530

    operator updated

---
+++ operator.py
@@ -0,0 +1,76 @@
+#operators in python 
+# PEMDEM
+
+# arithmetic operator 
+a=10
+b=8
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a//b)
+print(a**b)
+
+#assignment operator 
+#arithmetic assignment operator 
+a=10
+a+=5
+print(a)
+a-=5
+print(a)
+a*=5
+print(a)
+a/=5
+print(a)
+a%=5
+print(a)
+a//=5
+print(a)
+a**=5
+print(a)
+
+
+#comparison operator 
+a=20
+b=10
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+
+#logical operator 
+# and
+# or
+#not
+a=35
+b=23
+print(a>=30 and b<30)
+print(a>=30 or b>30)
+print(not(a>=30 and b>30))
+
+
+# bitwise operator 
+
+""""
+ & and
+ | or
+ ^ xor
+ ~ not
+ << zero fil left shift 
+ >> right shift 
+"""
+
+a=23
+b=13
+print(a&b)
+print(a|b)
+print(a^b)
+print(~a)
+print(a<<2)
+print(a>>2)
+
+
