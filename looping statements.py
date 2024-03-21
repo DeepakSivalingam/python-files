6ad3c3be18b97de22f66f367d319dfa03e052839
DeepakSivalingam <deepaksivalingam5@gmail.com>
2024-03-21 22:39:27 +0530

    looping updated

---
+++ looping statements.py
@@ -0,0 +1,18 @@
+#looping statements
+# for 
+# while 
+
+
+#printing the even number 
+i=2
+while i<=10:
+    print(i)
+    i+=2
+    
+#printing the odd numbers 
+i=1
+while i<10:
+    print(i)
+    i+=2
+    
+#for loop 
