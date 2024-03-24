bc6fbc03a18b7de41db28a67fa5d053750a5346c
DeepakSivalingam <deepaksivalingam5@gmail.com>
2024-03-24 10:57:14 +0530

    looping statements updated

--- looping statements.py
+++ looping statements.py
@@ -14,5 +14,39 @@ i=1
 while i<10:
     print(i)
     i+=2
-    
+
+#range in python 
+
+a=list(range(2,5)) # range(n,n-1)
+print(a)
+
+a=list(range(0,30,2))
+b=list(range(1,30,2))
+print(a)
+print(b)
+
 #for loop 
+for i in range(5):
+    print(i)
+
+for i in range(3):
+    x=int(input("enter the number : "))
+    y=int(input("enter the number : "))
+    print(x+y)
+    
+
+# nested for loop 
+
+for i in range(6):
+    for j in range(i):
+        print("*",end="")
+        
+    print("")
+    
+print("------------------------------------------------------------------")
+
+
+for i in range(5,0,-1):
+    for j in range(i):
+        print("*",end="")
+    print("")
