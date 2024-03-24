76a59c528d7d9d8b7e19acb27c293be721651887
DeepakSivalingam <deepaksivalingam5@gmail.com>
2024-03-24 20:36:05 +0530

    math functions updated

---
+++ math functions.py
@@ -0,0 +1,10 @@
+import math
+
+print(math.sqrt(9))
+print(math.floor(1.9))
+print(math.ceil(1.3))
+print(math.factorial(9))
+print(math.fabs(-9))
+print(math.pow(9,2))
+print(math.pi)
+print(math.e)
