8675b4d7248ebb4d3ca9970b1ccf1cccd5a1f43f
DeepakSivalingam <deepaksivalingam5@gmail.com>
2024-03-31 17:57:37 +0530

    updated

--- file.py
+++ file.py
@@ -3,4 +3,19 @@
 
 #two modes-read and write
 
-file=open("test file.txt","r") 
+try:
+    file=open("test file.txt","r") 
+    print(file.read())
+except FileNotFoundError:
+    print("file not found error")
+else:
+    file.close()
+    
+    
+try:
+    file=open("test file.txt","r") 
+    print(file.readline())     # this readline() function reads the data by single line 
+except FileNotFoundError:
+    print("file not found error")
+else:
+    file.close()
