95c0369d5728dc2f4e3a32aa798a4cc8c04e3428
DeepakSivalingam <deepaksivalingam5@gmail.com>
2024-03-31 20:04:10 +0530

    updated

--- file.py
+++ file.py
@@ -14,8 +14,49 @@ else:
     
 try:
     file=open("test file.txt","r") 
-    print(file.readline())     # this readline() function reads the data by single line 
+    #print(file.readline())     # this readline() function reads the data by single line 
+    #print(file.readline(2))     # this readline(2) function reads the first two letter of the next line
+    print(file.readlines())     # it converts the content of the file into list 
 except FileNotFoundError:
     print("file not found error")
 else:
     file.close()
+    
+    
+    #looping the contents of the file 
+    
+    file=open("test file.txt","r") 
+    for x in file:
+        print(x)
+        
+    #write to an exixting file
+    
+file=open("test file.txt",'w')
+file.write("this is the content for writing to an existing file ")   #here we overwrite the values
+file=open("test file.txt","r")
+print(file.read())
+
+#appending to the file 
+
+file=open("test file.txt",'a')
+file.write("\nthis is the append operation ")   #here we overwrite the values
+file=open("test file.txt","r")
+print(file.read())
+
+#deleting the file 
+
+#to deleting the file to import the os
+
+import os
+if os.path.exists("test.txt"):
+    os.remove("test.txt")
+else:
+    print("file not found !")
+
+#for deleting the folder
+
+import os
+if os.path.exists("sample"):
+    os.rmdir("sample")
+else:
+    print("folder not found !")
